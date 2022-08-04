0fdd34580f906bbcb1d2de795c6711a77a5ba784
Daniel Ramirez <contact@dann.dev>
2022-08-04 11:47:34 -0500

    Update readme file

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -7,7 +7,7 @@ afterAll(() => {
 });
 
 describe('Test endpoint responses', () => {
-  it('gets the / endpoint', async () => {
+  it('gets the / endpoint', async (done: DoneFn) => {
     const response = await request.get('/api');
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
@@ -21,5 +21,6 @@ describe('Test endpoint responses', () => {
         },
       ],
     });
+    done();
   });
 });
